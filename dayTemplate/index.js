aa2ed6da748ddbcaa5e482ee65297c462c079fe1
Casey Holzer <cholzer@unleashed-technologies.com>
2020-12-19 13:32:26 -0700

    update dayTemplate

--- dayTemplate/index.js
+++ dayTemplate/index.js
@@ -1,9 +1,10 @@
 import getInput from "../../util/getInput.js";
 
 export const part1 = () => {};
+export const part2 = () => {};
 
 const input = getInput(import.meta.url);
 
-// console.log("--DAY 10--");
+// console.log("--DAY X--");
 // console.log("PART 1", part1(input));
 // console.log("PART 2", part2(input));
