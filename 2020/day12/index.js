ce4029d25bfb9f211d7e7e54ef015157b8063a83
Casey Holzer <cholzer@unleashed-technologies.com>
2020-12-20 10:36:31 -0700

    day 12 part 1

---
+++ 2020/day12/index.js
@@ -0,0 +1,65 @@
+import getInput from "../../util/getInput.js";
+
+const directions = ["E", "S", "W", "N"];
+
+const turnMap = {
+	R: 1,
+	L: -1,
+};
+
+const directionMap = {
+	E: [0, 1],
+	W: [0, -1],
+	N: [1, 1],
+	S: [1, -1],
+};
+
+const parseLine = (line, direction, coordinates) => {
+	const action = line.slice(0, 1);
+	const value = Number(line.slice(1));
+
+	switch (action) {
+		case "F": {
+			let [index, multiplier] = directionMap[direction];
+
+			coordinates[index] += value * multiplier;
+			break;
+		}
+		case "R":
+		case "L": {
+			let directionIndex = directions.indexOf(direction);
+			let newIndex = directionIndex + (value / 90) * turnMap[action];
+
+			direction = directions.slice(newIndex % directions.length)[0];
+			break;
+		}
+		default: {
+			let [index, multiplier] = directionMap[action];
+
+			coordinates[index] += value * multiplier;
+		}
+	}
+
+	return [direction, coordinates];
+};
+
+export const part1 = (input) => {
+	let direction = "E";
+	let coordinates = [0, 0];
+
+	for (let i = 0; i < input.length; i++) {
+		const line = input[i];
+
+		[direction, coordinates] = parseLine(line, direction, coordinates);
+	}
+
+	return coordinates.reduce((a, b) => Math.abs(a) + Math.abs(b));
+};
+
+export const part2 = (input) => {};
+
+const input = getInput(import.meta.url);
+
+console.log("--DAY 12--");
+console.log("PART 1", part1(input));
+// console.log("PART 2", part2(input));
