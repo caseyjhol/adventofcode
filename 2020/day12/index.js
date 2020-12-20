e6fadb59a6f7c3b56dcad3cc67fd2143259f6a1e
Casey Holzer <cholzer@unleashed-technologies.com>
2020-12-20 13:13:25 -0700

    day 12 part 2

--- 2020/day12/index.js
+++ 2020/day12/index.js
@@ -14,7 +14,7 @@ const directionMap = {
 	S: [1, -1],
 };
 
-const parseLine = (line, direction, coordinates) => {
+const parseLine1 = (line, direction, coordinates) => {
 	const action = line.slice(0, 1);
 	const value = Number(line.slice(1));
 
@@ -50,16 +50,59 @@ export const part1 = (input) => {
 	for (let i = 0; i < input.length; i++) {
 		const line = input[i];
 
-		[direction, coordinates] = parseLine(line, direction, coordinates);
+		[direction, coordinates] = parseLine1(line, direction, coordinates);
 	}
 
 	return coordinates.reduce((a, b) => Math.abs(a) + Math.abs(b));
 };
 
-export const part2 = (input) => {};
+const parseLine2 = (line, waypoint, coordinates) => {
+	const action = line.slice(0, 1);
+	const value = Number(line.slice(1));
+
+	switch (action) {
+		case "F": {
+			coordinates = [
+				waypoint[0] * value + coordinates[0],
+				waypoint[1] * value + coordinates[1],
+			];
+			break;
+		}
+		case "R":
+		case "L": {
+			for (let i = 0; i < value / 90; i++) {
+				waypoint = [
+					turnMap[action] * waypoint[1],
+					turnMap[action] * -1 * waypoint[0],
+				];
+			}
+			break;
+		}
+		default: {
+			let [index, multiplier] = directionMap[action];
+
+			waypoint[index] += value * multiplier;
+		}
+	}
+
+	return [waypoint, coordinates];
+};
+
+export const part2 = (input) => {
+	let waypoint = [10, 1];
+	let coordinates = [0, 0];
+
+	for (let i = 0; i < input.length; i++) {
+		const line = input[i];
+
+		[waypoint, coordinates] = parseLine2(line, waypoint, coordinates);
+	}
+
+	return coordinates.reduce((a, b) => Math.abs(a) + Math.abs(b));
+};
 
 const input = getInput(import.meta.url);
 
 console.log("--DAY 12--");
 console.log("PART 1", part1(input));
-// console.log("PART 2", part2(input));
+console.log("PART 2", part2(input));
--- 2020/day12/tests/index.js
+++ 2020/day12/tests/index.js
@@ -3,16 +3,14 @@ import { part1, part2 } from "../index.js";
 
 const input = getInput(import.meta.url);
 
-console.log(part2(input));
-
 if (part1(input) === 25) {
 	console.log("PART 1", "PASS");
 } else {
 	console.log("PART 1", "FAIL");
 }
 
-/*if (part2(input) === 19208) {
+if (part2(input) === 286) {
 	console.log("PART 2", "PASS");
 } else {
 	console.log("PART 2", "FAIL");
-} */
+}
